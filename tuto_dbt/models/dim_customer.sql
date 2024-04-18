3d0be6c412a22974c7e45c089a4e965d0c965f94
Edu <88285051+Edu963@users.noreply.github.com>
2024-04-18 11:55:22 +0200

    new file reviewed

--- tuto_dbt/models/dim_customer.sql
+++ tuto_dbt/models/dim_customer.sql
@@ -5,11 +5,11 @@ with sq as (
 	 stg_store.*,
 	 stg_customer.*
 	
-	 from dwh_staging.stg_store 
-	 left join dwh_staging.stg_customer  on dwh_staging.stg_customer.store_id  = dwh_staging.stg_store.business_id 
+	 from {{ ref ('stg_customer')}}
+	 left join {{ ref ('stg_store')}}  on stg_customer.store_id  = stg_store.business_id 
 )
 select 
 	sq.*,
 	stg_person.*
 from sq
-left join dwh_staging.stg_person on dwh_staging.stg_person.business_entity_id = sq.person_id
+left join {{ ref ('stg_person')}}stg_person on stg_person.business_entity_id = sq.person_id
